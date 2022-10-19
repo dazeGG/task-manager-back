042bf1900f6ef5a51bf08e5c219ec358bfa60fb5
Vladimir <86754494+dazeGG@users.noreply.github.com>
2022-10-20 01:16:24 +0300

    added options method

--- middlewares/setHeaders.js
+++ middlewares/setHeaders.js
@@ -1,6 +1,9 @@
 module.exports = (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, OPTIONS, DELETE"
+  );
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type"
